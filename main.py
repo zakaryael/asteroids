3da60ab0007fd294421d369fb02e4eb23fd68924
zakarya <zakaria.elkhiyati@gmail.com>
2024-08-19 21:05:14 +0200

    Add game clock and redering according to a framerate for a smoother experience

--- main.py
+++ main.py
@@ -5,12 +5,17 @@ def main():
     print(f"Starting asteroids! \nScreen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    game_clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
         pygame.display.flip()
+        dt = game_clock.tick(FPS) / 1000
 
 if __name__ == "__main__":
           main()
