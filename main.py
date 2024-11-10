b49d5ee8510702f8036bd1281137e2da41eae58d
zakarya <zakaria.elkhiyati@gmail.com>
2024-11-10 22:15:10 +0100

    Integrate player, asteroid, and shot classes into the main game loop with collision detection

--- main.py
+++ main.py
@@ -1,21 +1,55 @@
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     print(f"Starting asteroids! \nScreen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # Create some groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = updatable, drawable
+    Asteroid.containers = updatable, drawable, asteroids
+    AsteroidField.containers = updatable
+    Shot.containers = updatable, drawable, shots
+    # Instantiate a player
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+    asteroid_field = AsteroidField()
+
+
     game_clock = pygame.time.Clock()
     dt = 0
 
+    # Game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        for sprite in updatable:
+            sprite.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
+        
+        for asteroid in asteroids:
+            if player.collides_with(asteroid):
+                print("You lose !")
+                return
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    asteroid.split()
+                    shot.kill()
         pygame.display.flip()
         dt = game_clock.tick(FPS) / 1000
+
 
 if __name__ == "__main__":
           main()
