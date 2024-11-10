b424ba6ea490dc7b50933b3dd57f1c7f39e77723
zakarya <zakaria.elkhiyati@gmail.com>
2024-11-10 22:15:01 +0100

    Add Shot class for projectile representation and movement

---
+++ shot.py
@@ -0,0 +1,14 @@
+from circleshape import CircleShape
+from constants import *
+import pygame
+
+class Shot(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, SHOT_RADIUS)
+        
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, (255, 255, 255), self.position, self.radius, 2)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
